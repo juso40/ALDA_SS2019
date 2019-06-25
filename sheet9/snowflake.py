3f455ba8696dda2527428242267c5d4957179cfb
juso40 <justin.sostmann@googlemail.com>
2019-06-25 14:16:43 +0200

    moar optimized

--- sheet9/snowflake.py
+++ sheet9/snowflake.py
@@ -12,6 +12,8 @@ def kochSnowflake(level):
         return coordinates
     else:
         XY = []
+        cos60 = math.cos(math.radians(60))#we will need cos and sin very often
+        sin60 = math.sin(math.radians(60))  
         for _ in range(level):
             for i in range(len(coordinates)):
 
@@ -19,19 +21,20 @@ def kochSnowflake(level):
                 p1X = p1[0] / 3
                 p1Y = p1[1] / 3
 
-                p2 = coordinates[(i+1) % len(coordinates)]
+                if i == len(coordinates) - 1:
+                    p2 = coordinates[0]
+                else:
+                    p2 = coordinates[i+1]
+                
                 p2X = p2[0] / 3
                 p2Y = p2[1] / 3
 
-                third1 = (2 * p1X + p2X, 2 * p1Y + p2Y)
-                third2 = (p1X + 2 * p2X, p1Y + 2 * p2Y)
+                third1 = (p1X + p1X + p2X, p1Y + p1Y + p2Y)
+                third2 = (p1X + p2X + p2X, p1Y + p2Y+ p2Y)
 
                 vec2DX = third2[0] - third1[0]
                 vec2DY = third2[1] - third1[1] 
-
-                cos60 = math.cos(math.radians(60))
-                sin60 = math.sin(math.radians(60))
-                
+ 
                 rotatedX = cos60 * vec2DX + sin60 * vec2DY
                 rotatedY = -sin60 * vec2DX + cos60 * vec2DY
 
