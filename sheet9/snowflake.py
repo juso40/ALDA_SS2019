c8e1e9aa5f983fa12e440db690aff7f141e5eae1
juso40 <justin.sostmann@googlemail.com>
2019-06-25 13:39:37 +0200

    optimized!!!!!!!!

--- sheet9/snowflake.py
+++ sheet9/snowflake.py
@@ -4,8 +4,8 @@ from math import sqrt
 
 def kochSnowflake(level):
     coordinates = [
-                    (0., 0.),
-                    (1., 0.),
+                    (0, 0),
+                    (1, 0),
                     (1/2, sqrt(3)/2)
                   ]
     if level == 0:
@@ -14,26 +14,33 @@ def kochSnowflake(level):
         XY = []
         for _ in range(level):
             for i in range(len(coordinates)):
-                newX = (coordinates[i][0] / 3) + (2 * coordinates[(i+1) % len(coordinates)][0] / 3)
-                newY = (coordinates[i][1] / 3) + (2 * coordinates[(i+1) % len(coordinates)][1] / 3)
-                third2 = (newX, newY)
 
-                newX = (2 * coordinates[i][0] / 3) + (coordinates[(i+1) % len(coordinates)][0] / 3)
-                newY = (2 * coordinates[i][1] / 3) + (coordinates[(i+1) % len(coordinates)][1] / 3)
-                third1 = (newX, newY)
+                p1 = coordinates[i]
+                p1X = p1[0] / 3
+                p1Y = p1[1] / 3
+
+                p2 = coordinates[(i+1) % len(coordinates)]
+                p2X = p2[0] / 3
+                p2Y = p2[1] / 3
+
+                third1 = (2 * p1X + p2X, 2 * p1Y + p2Y)
+                third2 = (p1X + 2 * p2X, p1Y + 2 * p2Y)
 
                 vec2DX = third2[0] - third1[0]
                 vec2DY = third2[1] - third1[1] 
+
+                cos60 = math.cos(math.radians(60))
+                sin60 = math.sin(math.radians(60))
                 
-                rotatedX = math.cos(math.radians(-60)) * vec2DX - math.sin(math.radians(-60)) * vec2DY
-                rotatedY = math.sin(math.radians(-60)) * vec2DX + math.cos(math.radians(-60)) * vec2DY
+                rotatedX = cos60 * vec2DX + sin60 * vec2DY
+                rotatedY = -sin60 * vec2DX + cos60 * vec2DY
 
                 midpoint = (rotatedX + third1[0], rotatedY + third1[1])
             
-                XY.extend([coordinates[i],
+                XY.extend((coordinates[i],
                             third1,
                             midpoint,
-                            third2])
+                            third2))
             coordinates = XY
         return XY
             
